df17182f187fd436748d307c0dc85e8b4b4ea51c
ADITYA <adityadubey772@gmail.com>
2022-07-17 14:29:49 +0530

    test

---
+++ function.js
@@ -0,0 +1,8 @@
+function showImage(){
+		    var img = document.getElementById("img");
+		    img.style.visibility = 'visible';
+		    }
+function hideImage(){
+		    var img = document.getElementById("img");
+		    img.style.visibility = 'hidden';
+		    }
